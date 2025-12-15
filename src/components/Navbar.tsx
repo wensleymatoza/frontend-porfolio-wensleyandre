9afd1891bc60e15f6a58232b11cba37ad9901875
Nihility <rbfmatoza@mymail.mapua.edu.ph>
2025-12-15 20:37:59 +0800

    feat: add navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,43 @@
+import { Link } from "@tanstack/react-router";
+
+export type navItem = {
+  name: string;
+  href: string;
+};
+
+const navItems: navItem[] = [
+  { name: "Home", href: "/" },
+  { name: "About", href: "/about" },
+  { name: "Projects", href: "/projects" },
+  { name: "Contact", href: "/contact" },
+];
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="sticky z-50 ">
+        <div className="flex border-2 p-2 justify-between mx-5 rounded-b-2xl">
+          <div>
+            <span className="text-cyan-300">Wensley Andre's </span> 
+            <span className="text-purple-300">Portfolio</span>
+          </div>
+
+          <div className="flex gap-x-3">
+            {navItems.map((item) => (
+              <Link 
+              key={item.name} 
+              to={item.href} 
+              className="hover:scale-105 ease-in-out transition-transform duration-200">
+                <h2>
+                    {item.name}
+                </h2>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,20 @@
-import { createFileRoute } from '@tanstack/react-router'
+import Navbar from '@/components/Navbar'
+import { createFileRoute} from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
+  head:() => ({
+    meta: [
+      {title: 'Home Page | Monarch'},
+    ]
+  })
 })
 
 function RouteComponent() {
-  return <div>Hello "/"!</div>
+  return (
+    <>
+    <Navbar />
+    </>
+  )
 }
 
