86a6e1839114ce4cdc897227976a95efeb476b53
Nihility <rbfmatoza@mymail.mapua.edu.ph>
2025-12-26 07:34:32 +0800

    style: add background css @homepage

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,15 +1,18 @@
 import { Link } from "@tanstack/react-router"
+import "../styles.css"
+
 
 const HomePage = () => {
   return (
     <>
-    <div className="flex min-h-screen border-2 justify-center items-center ">
-      <div className = 'flex border-2 size-[500px] p-2 m-2 justify-center items-center flex-col '>
+    <div className="flex flex-col  ">
+    <div className="flex min-h-screen min-w-screen border-2 justify-center items-center bg-pattern1 ">
+      <div className = 'flex border-2 size-125 p-2 m-2 justify-center items-center flex-col '>
         <span><h2>I am Frontend Webdeveloper</h2></span>
-        <a href= '#portfolio'>See Portfolio</a>
+        <a href= '#portfolio' className="flex border-2 p-2 m-2 rounded-2xl hover:scale-105 bg-blue-300">See Portfolio</a>
       </div>
-      <div className="flex border-2 size-[500px] p-2 m-2">
-          
+      <div className="flex border-2 size-125 p-2 m-2">
+                  {/* first section */}
       </div>
     </div>
     <div className="min-h-screen border-2 ">
@@ -17,7 +20,9 @@ const HomePage = () => {
     </div>
 
     <div id = 'portfolio'className="min-h-screen border-2 ">
-        Sub branch
+      {/* second section */}
+      <h1>this is the 2nd section</h1>
+    </div>
     </div>
     </>
   )
