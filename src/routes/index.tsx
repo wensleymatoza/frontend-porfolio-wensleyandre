d9ba823071a53070fed473cdef9e4ef8780fabe3
Nihility <rbfmatoza@mymail.mapua.edu.ph>
2026-01-03 20:55:54 +0800

    style:add css to homepage for cleaner code

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 
-import HomePage from '@/components/HomePage'
-import Navbar from '@/components/Navbar'
+import HomePage from '@/components/Homepage/HomePage'
+import Navbar from '@/components/Navbar/Navbar'
 import { createFileRoute} from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
